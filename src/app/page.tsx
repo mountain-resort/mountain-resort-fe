572d8e9160876ba0e12b251012b941b4b82df35a
yoolri <lucca6042@gmail.com>
2025-02-14 15:36:49 +0900

    :art: [Style] global.css 초기 설정
    + 테일윈드 사용 예시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
+    // 기본 폰트 lora, open sans 쓸 때만 font-sans 주면 됨. 
+    // Primary - MR_white, black, gold / Secondary - dark_grey, light_grey
+    // fontSize - xl : 72px, 2lg: 48px, lg: 32px, md: 24px, sm: 18px, xs: 16px
+    // fontWeight - bold: 700, semibold: 600, medium: 500, regular: 400
+    // screens - mobile: 375px, pc: 1440px
     <div className='bg-primary-gold'>
       <div className='underline p-10'>
-        <h1 className='text-xl font-bold text-primary-black font-lora'>
+        <h1 className='text-xl font-bold text-primary-black font-lora pc:text-sm'>
           This is Landing page
         </h1>
-        <h1 className='text-2lg text-secondary-light_grey font-regular font-sans'>
+        <h1 className='text-2lg text-secondary-light_grey font-regular font-sans pc:text-md'>
           This is Landing page
         </h1>
       </div>
-      <h1> This is Landing page </h1>
+      <h1 className='pc:text-xl'> This is Landing page </h1>
     </div>
   );
 }
