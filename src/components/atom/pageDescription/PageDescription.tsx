eb12598693c5d181ec51fce6af8a8efe30380dec
yoolri <lucca6042@gmail.com>
2025-02-20 11:30:14 +0900

    :sparkles: [Feat] 페이지 설명 - 컴포넌트 영역 추가
    + storybook Docs 추가

--- src/components/atom/pageDescription/PageDescription.tsx
+++ src/components/atom/pageDescription/PageDescription.tsx
@@ -4,16 +4,28 @@ interface DescriptionType {
   title?: string;
   subTitle?: string;
   content?: string;
+  compo?: React.ReactNode;
 }
 
-const PageDescription = ({ title, subTitle, content }: DescriptionType) => {
+const PageDescription = ({
+  title,
+  subTitle,
+  content,
+  compo,
+}: DescriptionType) => {
   return (
-    <div className='bg-primary-white px-16 py-40 flex flex-col gap-[40px]'>
-      {title && <div className='text-md'>{title}</div>}
-      <div className='flex flex-col gap-[17px] font-sans'>
-        {subTitle && <div className='text-xs'>{subTitle}</div>}
-        {content && <div className='text-2xs'>{content}</div>}
-      </div>
+    <div className='bg-primary-white'>
+      {compo ? (
+        compo
+      ) : (
+        <div className='px-[16px] py-[40px] flex flex-col gap-[40px]'>
+          {title && <div className='text-md'>{title}</div>}
+          <div className='flex flex-col gap-[17px] font-sans'>
+            {subTitle && <div className='text-xs'>{subTitle}</div>}
+            {content && <div className='text-2xs'>{content}</div>}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
