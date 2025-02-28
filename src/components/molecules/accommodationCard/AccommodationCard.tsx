f37ebd1d40d47437847baf90aeec1a821e1817dd
minseokim <claudia503@naver.com>
2025-02-28 15:14:45 +0900

    :art: [Style] 숙소 카드 스타일 수정

--- src/components/molecules/accommodationCard/AccommodationCard.tsx
+++ src/components/molecules/accommodationCard/AccommodationCard.tsx
@@ -14,22 +14,29 @@ const styles = {
   imageWrapper: 'relative min-h-48 pc:min-h-60',
   content: 'font-sans px-6 py-4 pc:px-8 pc:py-6',
   textContainer: 'flex flex-col items-center text-center gap-6',
-  buttonContainer: 'flex justify-center items-center w-full space-x-4 pc:space-x-7 mt-5 pc:mt-6',
-  separatorLine: 'border-t-2 border-primary-gold border-solid w-[51px] hidden pc:block',
+  buttonContainer:
+    'flex justify-center items-center w-full space-x-4 pc:space-x-7 mt-5 pc:mt-6',
+  separatorLine:
+    'border-t-2 border-primary-gold border-solid w-[51px] hidden pc:block',
   buttonStyles: 'h-[35px] pc:h-[43px] text-[14px]',
 };
 
-const AccommodationCard = ({ name, description, images }: AccommodationCardProps) => {
-  const imageSrc = images[0] ?? assets.images.accommodation01FamilyFriendlyCabin;
+const AccommodationCard = ({
+  name,
+  description,
+  images,
+}: AccommodationCardProps) => {
+  const imageSrc =
+    images[0] ?? assets.images.accommodation01FamilyFriendlyCabin;
 
   return (
     <div className={styles.card}>
       <div className={styles.imageWrapper}>
         <Image
           src={imageSrc}
-          alt="랜딩페이지에 들어가는 숙박카드"
+          alt='랜딩페이지에 들어가는 숙박카드'
           fill
-          className="object-cover"
+          className='object-cover'
         />
       </div>
 
@@ -37,9 +44,9 @@ const AccommodationCard = ({ name, description, images }: AccommodationCardProps
         <div className={styles.textContainer}>
           <TextBox
             text={name}
-            weight="font-semibold"
-            size="text-base pc:text-lg"
-            otherStyle="font-lora"
+            weight='font-semibold'
+            size='text-base pc:text-lg'
+            otherStyle='font-lora'
           />
 
           <div className={styles.separatorLine}></div>
@@ -47,17 +54,20 @@ const AccommodationCard = ({ name, description, images }: AccommodationCardProps
           {description && (
             <TextBox
               text={description}
-              size="text-xs pc:text-sm"
-              otherStyle="text-primary-gray line-clamp-2 pc:line-clamp-none"
+              size='text-xs pc:text-sm'
+              otherStyle='text-primary-gray line-clamp-2 pc:line-clamp-none'
             />
           )}
         </div>
 
         <div className={styles.buttonContainer}>
-          <Button variant="primary" className={styles.buttonStyles}>
+          <Button variant='primary' className={styles.buttonStyles}>
             ROOM DETAILS
           </Button>
-          <Button variant="primary" className={`${styles.buttonStyles} hidden pc:flex`}>
+          <Button
+            variant='primary'
+            className={`${styles.buttonStyles} hidden pc:flex`}
+          >
             CHECK RATE
           </Button>
         </div>
@@ -67,4 +77,3 @@ const AccommodationCard = ({ name, description, images }: AccommodationCardProps
 };
 
 export default AccommodationCard;
-
