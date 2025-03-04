64cbe0620d4eea6494f7292c64b12e96bdbe2cbb
minseokim <claudia503@naver.com>
2025-03-04 21:51:04 +0900

    :bug: [Fix] booking 카드 글자 크기 고정 값으로 수정

--- src/components/molecules/bookingCard/BookingCard.tsx
+++ src/components/molecules/bookingCard/BookingCard.tsx
@@ -24,55 +24,55 @@ const BookingCard = ({
     images[0] ?? assets.images.accommodation01FamilyFriendlyCabin;
 
   return (
-    <div className='w-full bg-white h-auto flex flex-col pc:flex-row border border-black border-solid pc:h-[375px]'>
-      <div className='relative w-full h-[189px] pc:w-1/2 pc:h-[375px] pc:flex-shrink-0 border-b-2 border-gray-300 pc:border-b-0'>
-        <Image src={imageSrc} alt={name} fill className='object-cover' />
+    <div className="w-full bg-white flex flex-col pc:flex-row border border-black border-solid min-h-[444px] pc:min-h-[375px]">
+      <div className="relative w-full h-[189px] pc:w-1/2 pc:min-h-[375px] flex-shrink-0 border-b-2 border-gray-300 pc:border-b-0">
+        <Image src={imageSrc} alt={name} fill className="object-cover" />
       </div>
 
-      <div className='flex flex-col w-full px-4 pt-6 pb-4 pc:px-8 pc:py-6 pc:w-1/2 min-h-[255px] pc:h-auto'>
+      <div className="flex flex-col w-full px-4 pt-[24px] pc:pt-[50px] pb-4 pc:px-8 pc:py-6 pc:w-1/2 min-h-[255px] pc:h-[375px]">
         <TextBox
           text={name}
-          otherStyle='font-semibold border-b border-gray-400 pb-2 underline pl-[12px] text-[17px] pc:text-[22px] text-center pc:text-left'
+          size="text-2xs pc:text-[21.94px]"
+          weight="font-medium"
+          otherStyle="border-b border-gray-400 pb-2 underline pl-[12px] text-center pc:text-left"
         />
 
-        <div className='flex justify-between w-full px-4 mt-6 pc:mt-[34px] h-[63px] pc:h-[60px]'>
-          <div className='flex flex-col text-left text-[clamp(11px,3vw,16px)] pc:text-sm gap-[6px]'>
-            <span className='font-semibold'>Occupancy:</span>
-            <p>{occupancy.join(', ')}</p>
+        <div className="flex justify-between w-full px-4 mt-[22px] pc:mt-[30px] h-[63px] pc:h-[60px]">
+          <div className="flex flex-col text-left text-3xs pc:text-xs gap-[6px]">
+            <span className="font-semibold">Occupancy:</span>
+            <p className="font-regular">{occupancy.join(', ')}</p>
           </div>
 
-          <div className='flex flex-col text-left text-[clamp(11px,3vw,16px)] pc:text-sm gap-[6px]'>
-            <span className='font-semibold'>Size:</span>
-            <p>{size.join(', ')}</p>
+          <div className="flex flex-col text-left text-3xs pc:text-xs gap-[6px]">
+            <span className="font-semibold">Size:</span>
+            <p className="font-regular">{size.join(', ')}</p>
           </div>
 
-          <div className='flex flex-col text-left text-[clamp(11px,3vw,16px)] pc:text-sm gap-[6px]'>
-            <span className='font-semibold'>Bedding:</span>
+          <div className="flex flex-col text-left text-3xs pc:text-xs gap-[6px]">
+            <span className="font-semibold">Bedding:</span>
             {bedding.map(([bedType, count], index) => (
-              <p key={`${bedType}-${count}-${index}`}>
+              <p key={`${bedType}-${count}-${index}`} className="font-regular">
                 {bedType} X {count}
               </p>
             ))}
           </div>
         </div>
 
-        <div className='border-t-2 border-gray-300 border-solid w-full mt-6 pc:mt-[28px]'></div>
+        <div className="border-t-2 border-gray-300 border-solid w-full mt-[22px] pc:mt-[28px]"></div>
 
-        <div className='flex justify-between items-center w-full px-4 mt-6 pc:mt-[31px] h-[73px]'>
-          <span className='text-[clamp(10px,3vw,15px)] pc:text-base'>
-            Price per night
-          </span>
-          <span className='text-[clamp(14px,3vw,19px)] pc:text-lg font-bold flex items-center'>
-            <span className='text-[clamp(13px,3vw,18px)] pc:text-sm mr-2'>
-              AUD
-            </span>
+        <div className="flex justify-between items-center w-full px-4 mt-[22px] pc:mt-[25px] h-[75px]">
+          <span className="text-xs pc:text-base font-regular">Price per night</span>
+          <span className="text-2xs pc:text-[20px] font-bold flex items-center">
+            <span className="text-xs pc:text-xs mr-2">AUD</span>
             {price}
           </span>
         </div>
 
-        <Button variant='primary' className='mt-4'>
+        <Button variant="primary" className="mt-[11px] pc:mt-[10px]">
           BOOK NOW
         </Button>
+
+        <div className="mb-[24px] pc:mb-[20px]"></div>
       </div>
     </div>
   );
